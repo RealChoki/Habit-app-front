360cd0efc9889380d59f197508bdc1134aff274b
RealChoki <123271772+RealChoki@users.noreply.github.com>
2024-05-08 20:03:49 +0200

    Remove unused code and update styles in AboutView, HomeView, and HelloWorld components

---
+++ habit app/src/components/HomeCalender.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="day">
+        <p>Mon</p>
+        <div class="day2">
+            <p>1</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .day {
+        width: 40px;
+        height: 50px;
+        border-radius: 15px;
+        background-color: #1A1A1B;
+        margin-left: 1em;
+        padding: 0;
+        border: 1px solid white;
+    }
+
+    .day p {
+        font-size: 10px;
+        margin-top: 0;
+        text-align: center;
+        padding-top: 5px
+    }
+
+    .day2 {
+        width: 40px;
+        height: 28.85px;
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+        border-top-right-radius: 8px;
+        border-top-left-radius: 8px;
+        background-color: #232323;
+        margin: 0;
+        padding: 0;
+        border: 1px solid white;
+    }
+
+    .day2 p {
+        font-size: 13px;
+        font-weight: bold;
+    }
+</style>
--- habit app/src/views/HomeView.vue
+++ habit app/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomeCalender from '@/components/HomeCalender.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<header>
+  <img class="menu-icon" alt="Menu Icon" src="../components/icons/Menu.png">
+  <h3>May 1, 2024</h3>
+</header>
+<HomeCalender />
 </template>
+
+<style scoped>
+  .menu-icon {
+  }
+
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 24px;
+    gap: 7px;
+  }
+
+</style>
