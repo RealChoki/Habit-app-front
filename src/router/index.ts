26282c3e30a8e13d4267ab0b18e20f2dac2f7755
davidcraccer <dsvoboda25@gmail.com>
2024-05-15 15:44:40 +0200

    update: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import EvaluateView from '@/views/EvaluateView.vue'
 import YesNoView from '@/views/YesNoView.vue'
+import NumericValueView from '@/views/NumericValueView.vue'
+import TimerView from '@/views/TimerView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,15 +37,15 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/EvaluateView.vue')
+      component: EvaluateView
     },
     {
-      path: '/yesnoView',
+      path: '/yesnoview',
       name: 'YesNoView',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/YesNoView.vue')
+      component: YesNoView
     },
     {
       path: '/numericvalueview',
@@ -51,7 +53,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/NumericValueView.vue')
+      component: NumericValueView
     },
     {
       path: '/timerview',
@@ -59,7 +61,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/TimerView.vue')
+      component: TimerView
     }
   ]
 })
