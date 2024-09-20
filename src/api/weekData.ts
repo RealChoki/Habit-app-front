2512a403ef51234b4c74883a36f504e1c8d6fe3b
davidcraccer <dsvoboda25@gmail.com>
2024-09-20 15:24:08 +0200

    update weekData API

--- src/api/weekData.ts
+++ src/api/weekData.ts
@@ -3,17 +3,10 @@ import apiClient from './axios'
 // Fetch all habits
 export const getWeekData = async (startDate: string, endDate: string) => {
   try {
-    console.log('Fetching days from', startDate, 'to', endDate)
-    const response = await apiClient.get('/', {
-      params: {
-        startDate,
-        endDate
-      }
-    })
-    console.log('Days fetched:', response.data)
-    return response.data
+    const response = await apiClient.get(`/week/${startDate}/${endDate}`);
+    return response.data;
   } catch (error) {
-    console.error('Error fetching week data:', error)
-    throw error
+    console.error('Error fetching week data:', error);
+    throw error;
   }
-}
+};
